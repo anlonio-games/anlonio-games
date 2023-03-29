268fbfe48f4f75d27ca18784c513ad159c4acea9
Anlonio <antonio.machado.dev@gmail.com>
2023-03-29 14:39:58 -0300

    update meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.ORIGIN || 'http://localhost:3000',
+    head: {
+      title: 'Anlonio Games',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'A nova era das lives de TFT' }
+      ]
+    }
+  },
   modules: [
     '@sidebase/nuxt-auth',
     '@nuxtjs/tailwindcss'
