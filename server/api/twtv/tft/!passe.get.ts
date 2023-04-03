a39a7327194d929cf64826fef47cf7e5629db09d
Anlonio <antonio.machado.dev@gmail.com>
2023-04-03 13:56:43 -0300

    add /me

--- server/api/twtv/tft/!passe.get.ts
+++ server/api/twtv/tft/!passe.get.ts
@@ -2,5 +2,5 @@ import moment from 'moment'
 
 export default eventHandler(() => {
   const daysLeft = moment('13/06/2023', 'DD/MM/YYYY').diff(moment().startOf('day'), 'days')
-  return `O passe acaba em ${daysLeft} dias. Saiba mais sobre as recompensas em: https://teamfighttactics.leagueoflegends.com/pt-br/news/game-updates/passe-glitch-total-e-mais/`
+  return `/me O passe acaba em ${daysLeft} dias. Saiba mais sobre as recompensas em: https://teamfighttactics.leagueoflegends.com/pt-br/news/game-updates/passe-glitch-total-e-mais/`
 })
--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,3 +1,3 @@
 export default eventHandler(() => {
-  return 'Link para o Patch atual: https://leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-13-6-do-teamfight-tactics/'
+  return '/me Link para o Patch atual: https://leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-13-6-do-teamfight-tactics/'
 })
