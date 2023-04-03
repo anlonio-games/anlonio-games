f559b67ae56c460f87d601aaab5dcf43033ab805
Anlonio <antonio.machado.dev@gmail.com>
2023-04-03 10:19:29 -0300

    add new commands

---
+++ server/api/twtv/tft/!passe.get.ts
@@ -0,0 +1,3 @@
+export default eventHandler(() => {
+  return 'O passe acaba em X dias. Saiba mais sobre as recompensas em: https://teamfighttactics.leagueoflegends.com/pt-br/news/game-updates/passe-glitch-total-e-mais/'
+})
---
+++ server/api/twtv/tft/!patch.get.ts
@@ -0,0 +1,3 @@
+export default eventHandler(() => {
+  return 'Link para o Patch atual: https://leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-13-6-do-teamfight-tactics/'
+})
