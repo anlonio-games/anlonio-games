8032b4ab16d1bb28131693c01bb5eb240590aca2
Anlonio <antonio.machado.dev@gmail.com>
2023-04-07 16:37:35 -0300

    new command stats+

--- server/api/twtv/tft/!stats.get.ts
+++ server/api/twtv/tft/!stats.get.ts
@@ -37,43 +37,9 @@ export default eventHandler(async (event) => {
     win: (augStats.count > 0 ? augStats.win / augStats.count * 100 : 0).toFixed(1)
   }
 
-  let prompt = `${name} (${rank}) ->
+  const prompt = `/me ${name} (${rank}) ->
   TOTAL:
-  Qtd: ${statsData.qtd} Avg: ${statsData.avg} Top4: ${statsData.top4} % Win: ${statsData.win} % |`
+  Nº Partidas: ${statsData.qtd} ; Média de posição: ${statsData.avg} ; Média de Top 4: ${statsData.top4} % ; Média de vitória: ${statsData.win} % |`
 
-  if (augStats.pick1) {
-    const pick1 = {
-      qtd: augStats.pick1.count.toLocaleString('pt-BR'),
-      avg: augStats.pick1.place.toFixed(2),
-      top4: (augStats.pick1.count > 0 ? augStats.pick1.top4 / augStats.pick1.count * 100 : 0).toFixed(1),
-      win: (augStats.pick1.count > 0 ? augStats.pick1.win / augStats.pick1.count * 100 : 0).toFixed(1)
-    }
-    prompt += ` 1ª Opção:
-    Qtd: ${pick1.qtd} Avg: ${pick1.avg} Top4: ${pick1.top4} % Win: ${pick1.win} % |`
-  }
-
-  if (augStats.pick2) {
-    const pick2 = {
-      qtd: augStats.pick2.count.toLocaleString('pt-BR'),
-      avg: augStats.pick2.place.toFixed(2),
-      top4: (augStats.pick2.count > 0 ? augStats.pick2.top4 / augStats.pick2.count * 100 : 0).toFixed(1),
-      win: (augStats.pick2.count > 0 ? augStats.pick2.win / augStats.pick2.count * 100 : 0).toFixed(1)
-    }
-    prompt += ` 2ª Opção:
-  Qtd: ${pick2.qtd} Avg: ${pick2.avg} Top4: ${pick2.top4} % Win: ${pick2.win} % |`
-  }
-
-  if (augStats.pick3) {
-    const pick3 = {
-      // count with millions separator
-      qtd: augStats.pick3.count.toLocaleString('pt-BR'),
-      avg: augStats.pick3.place.toFixed(2),
-      top4: (augStats.pick3.count > 0 ? augStats.pick3.top4 / augStats.pick3.count * 100 : 0).toFixed(1),
-      win: (augStats.pick3.count > 0 ? augStats.pick3.win / augStats.pick3.count * 100 : 0).toFixed(1)
-    }
-    prompt += ` 3ª Opção:
-  Qtd: ${pick3.qtd} Avg: ${pick3.avg} Top4: ${pick3.top4} % Win: ${pick3.win} % |`
-  }
-
-  return prompt + ` Para ver a descrição, digite !aug ${name}`
+  return prompt + ` Para dados completos, user !stats+. Para ver a descrição, digite !aug ${name}`
 })
