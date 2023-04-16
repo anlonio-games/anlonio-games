906f2bd03c72b831a2a949d966896359782b3725
Anlonio <antonio.machado.dev@gmail.com>
2023-04-16 15:44:33 -0300

    Level up command

---
+++ server/api/twtv/tft/!levelup.get.ts
@@ -0,0 +1,23 @@
+export default eventHandler((event) => {
+  const { search } : { search?: string} = getQuery(event)
+
+  const help = `Saiba quanto de EXP você vai receber usando o Aprimoramento NOVO NÍVEl ->
+  Digite o custo em ouro para subir para o próximo nível. Exemplo: !levelup 8`
+
+  if (!search || search === 'help') {
+    return help
+  }
+
+  const gold = parseInt(search)
+
+  if (isNaN(gold)) {
+    return help
+  }
+
+  // se valor não for exato
+  if (gold % 4 !== 0) {
+    return 'O custo em ouro deve ser múltiplo de 4.'
+  }
+
+  return `/me ${gold} de ouro = ${gold / 4 * 7} de EXP com Novo Nível`
+})
---
+++ server/api/twtv/tft/!lu.get.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import handler from './!levelup.get'
+
+export default handler
