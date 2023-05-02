31c411e53535ea60b10419d876417b7da9e86025
anlonio <anloniob@gmail.com>
2023-05-02 18:02:51 -0400

    update patch link

--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,3 +1,3 @@
 export default eventHandler(() => {
-  return '/me Link para as notas de atualização do patch 13.7, que entra amanhã: https://leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-13-7-do-teamfight-tactics/'
+  return '/me Link para as notas de atualização do patch 13.9: https://riot.com/42jiM9f'
 })
