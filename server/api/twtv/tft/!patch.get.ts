d336399dd2301ed6ab56bb8f25ac362a60675ebf
Anlonio <antonio.machado.dev@gmail.com>
2023-05-05 11:39:39 -0300

    update patch text

--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,3 +1,3 @@
 export default eventHandler(() => {
-  return '/me Link para as notas de atualização do patch 13.9, que irá para o live no dia 03/05: riot.com/41UlDFV'
+  return '/me Link para as notas de atualização do patch 13.9: riot.com/41UlDFV'
 })
