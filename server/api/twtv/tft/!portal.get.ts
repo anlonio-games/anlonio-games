a61dc2143b57badfa1bfb87ffd04f12831237613
anlonio <anloniob@gmail.com>
2023-06-22 20:57:26 -0300

    fix portal command

--- server/api/twtv/tft/!portal.get.ts
+++ server/api/twtv/tft/!portal.get.ts
@@ -2,7 +2,7 @@ import portals from '@/assets/portals.json'
 export default eventHandler((event) => {
   const { search, id } : { search?: string, id: number} = getQuery(event)
   
-  if (!search || search === 'help') {
+  if ((!search || search === 'help') && !id) {
     return 'Digite !portal [nome do portal] para obter informações sobre o portal. Exemplo: !portal o sumidouro | Essa api é mantida pelo Anlonio, para mais informações acesse: anlonio.games'
   }
 
