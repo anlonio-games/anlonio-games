d3d9d3f67630fffe79b15fe1defa415cb955a705
anlonio <anloniob@gmail.com>
2023-06-22 19:07:57 -0300

    get portal by id

--- server/api/twtv/tft/!portal.get.ts
+++ server/api/twtv/tft/!portal.get.ts
@@ -1,14 +1,14 @@
 import portals from '@/assets/portals.json'
 export default eventHandler((event) => {
   const { search, id } : { search?: string, id: number} = getQuery(event)
-
+  
   if (!search || search === 'help') {
     return 'Digite !portal [nome do portal] para obter informações sobre o portal. Exemplo: !portal o sumidouro | Essa api é mantida pelo Anlonio, para mais informações acesse: anlonio.games'
   }
 
   let portal
 
-  if (!search && id) {
+  if ((!search || search === 'help') && id) {
     if (isNaN(id) || (id < 0 || id >= portals.length)) {
       setResponseStatus(event, 404, 'Portal Doesn\'t exists')
       return 'Portal não encontrado. Digite o nome exato ou apenas as letras Maiúsculas (O Sumidouro = OS)'
