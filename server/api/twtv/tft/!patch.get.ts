c6fe30e8c022af53ca10ec9bfbdf5cc58cec5ee8
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-08-16 10:36:12 -0300

    chore: bump patch notes from 13.15 to 13.16

--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,3 +1,4 @@
 export default eventHandler(() => {
-  return '/me Link para as notas de atualização do patch 13.15: https://riot.com/45bsKLP'
+  // eslint-disable-next-line no-template-curly-in-string
+  return '/announce ${sender.name} As notas da atualização 13.16 já estão no site -> bit.ly/3KIe3HA'
 })
