b5e91896104cf5fe9eaa7920395918c4c0535219
Bruno Donatelli <bruno.donatelli17@gmail.com>
2023-08-16 12:59:16 -0300

    fix: remove sender name reference

--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,4 +1,3 @@
 export default eventHandler(() => {
-  // eslint-disable-next-line no-template-curly-in-string
-  return '/announce ${sender.name} As notas da atualização 13.16 já estão no site -> bit.ly/3KIe3HA'
+  return '/announce As notas da atualização 13.16 já estão no site -> bit.ly/3KIe3HA'
 })
