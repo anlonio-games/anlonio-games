cff6dadd465ca6a93b51069f19aa58236d3a2590
Bruno Donatelli <42386513+brunoredes@users.noreply.github.com>
2023-08-31 12:40:00 -0300

    Develop (#6)
    * chore: bump tft patch to 13.17
    
    * fix: add a new message in aug command

--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,3 +1,3 @@
 export default eventHandler(() => {
-  return '/announce As notas da atualização 13.16 já estão no site -> bit.ly/3KIe3HA'
+  return '/announce As notas da atualização 13.17 já estão no site -> bit.ly/3Ei4FXC'
 })
