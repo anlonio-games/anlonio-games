ef65b676666458569ea4b004eba5509310b92bd2
Bruno Donatelli <42386513+brunoredes@users.noreply.github.com>
2023-09-16 23:40:49 -0300

    fix: patch and pass end date

--- server/api/twtv/tft/!passe.get.ts
+++ server/api/twtv/tft/!passe.get.ts
@@ -1,6 +1,6 @@
 import moment from 'moment'
 
 export default eventHandler(() => {
-  const daysLeft = moment('06/12/2023', 'DD/MM/YYYY').diff(moment().startOf('day'), 'days')
+  const daysLeft = moment('20/11/2023', 'DD/MM/YYYY').diff(moment().startOf('day'), 'days')
   return `/me O passe acaba em ${daysLeft} dias ⋮-> Saiba mais sobre as recompensas em: http://bit.ly/3RkMatx`
 })
--- server/api/twtv/tft/!patch.get.ts
+++ server/api/twtv/tft/!patch.get.ts
@@ -1,4 +1,4 @@
-import { currentPatch, currentPatchLink } from 'server/api/helper'
+import { currentPatch, currentPatchLink } from '../../api/helper'
 
 export default eventHandler(() => {
   return `/announce As notas da atualização ${currentPatch} já estão no site -> ${currentPatchLink}`
