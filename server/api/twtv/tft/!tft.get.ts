58652d0b1200a10d18bc63adb7bf900c1cf4961a
Anlonio <antonio.machado.dev@gmail.com>
2023-10-19 19:55:59 -0300

    chore: fix channel missing

--- server/api/twtv/tft/!tft.get.ts
+++ server/api/twtv/tft/!tft.get.ts
@@ -1,14 +1,20 @@
 export default eventHandler(async (event) => {
   const { channel: chanelName } = getQuery(event)
 
-  const channel: any = await $fetch(`https://api.streamelements.com/kappa/v2/channels/${chanelName}`)
+  let aliases = []
 
-  const commands: any = await $fetch(`https://api.streamelements.com/kappa/v2/bot/commands/${channel._id}/public`)
+  if (chanelName) {
+    const channel: any = await $fetch(`https://api.streamelements.com/kappa/v2/channels/${chanelName}`)
 
-  const aliases = commands.find((command: any) => command.command === 'tft').aliases || []
+    const commands: any = await $fetch(`https://api.streamelements.com/kappa/v2/bot/commands/${channel._id}/public`)
 
-  if (aliases.length === 0) {
-    return 'Essa live não possui comandos do anlonio.games disponíveis'
+    aliases = commands.find((command: any) => command.command === 'tft').aliases || []
+
+    if (aliases.length === 0) {
+      return 'Essa live não possui comandos do anlonio.games disponíveis'
+    }
+  } else {
+    aliases = ['proximo', 'ixtal', 'info', 'aprimoramento', 'portal', 'passe', 'patch', 'baralho']
   }
 
   return `Essa live possui os seguintes comandos da Anlonio Games: ${aliases.join(', ')}`
