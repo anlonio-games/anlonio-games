d79a61fc297ca8d6447cad1cfc7af71493279d8b
Anlonio <antonio.machado.dev@gmail.com>
2023-10-20 18:32:35 -0300

    chore: add command !gincana

---
+++ server/api/twtv/tft/!gincana.get.ts
@@ -0,0 +1,3 @@
+export default eventHandler(() => {
+  return '/me De olho numa pequena lenda? No ProLegends tem rolado desafios com premiações! Para saber mais, esteja na transmissão oficial da Riot Games Brasil e use o comando !gincanaPrint ou !gincanaMeme.'
+})
