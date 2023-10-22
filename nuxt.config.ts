f2df11af405ffaffb6b6557c39ec5e382e2898ba
Anlonio <antonio.machado.dev@gmail.com>
2023-10-22 11:23:33 -0300

    chore: update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: 'vercel',
-    plugins: ['~/server/plugins/0.response.ts']
-  },
   app: {
     head: {
       title: 'Anlonio Games',
