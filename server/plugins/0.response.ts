d84e7a27ac0a1cc61aeb27504dac835bfb355f38
Anlonio <antonio.machado.dev@gmail.com>
2023-10-22 10:28:12 -0300

    ...

--- server/plugins/0.response.ts
+++ server/plugins/0.response.ts
@@ -1,7 +1,7 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('beforeResponse', (event, context: { body: string }) => {
     if (typeof context.body !== 'string') {
-      context.body = 'Bip bop bip... Acho que não to bem não 🤖'
+      context.body = 'Bip bop bip... Acho que não to bem não 🤖...'
     }
 
     if (event.context.plataform !== 'twitch') {
