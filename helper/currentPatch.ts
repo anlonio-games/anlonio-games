70675f6b4bddd4af44727c1aa70963a1c6f26ba5
Anlonio <antonio.machado.dev@gmail.com>
2024-01-09 17:59:18 -0300

    chore: patch 14.1 fix

--- helper/currentPatch.ts
+++ helper/currentPatch.ts
@@ -1,2 +1,2 @@
-export const currentPatch = '13.24' as const
-export const currentPatchLink = 'bit.ly/3RtAFPY' as const
+export const currentPatch = '14.1' as const
+export const currentPatchLink = 'riot.com/48p0uql' as const
--- server/api/helper/currentPatch.ts
+++
@@ -1,2 +0,0 @@
-export const currentPatch = '14.1'
-export const currentPatchLink = 'riot.com/48p0uql'
