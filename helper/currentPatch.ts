bf6efa4645fb7f43a3a3b9b0bcf11e6d7838f34e
Bruno Donatelli <42386513+brunoredes@users.noreply.github.com>
2024-01-23 19:01:14 -0300

    chore: bump tft version from 14.1 to 14.2

--- helper/currentPatch.ts
+++ helper/currentPatch.ts
@@ -1,2 +1,2 @@
-export const currentPatch = '14.1' as const
-export const currentPatchLink = 'riot.com/48p0uql' as const
+export const currentPatch = '14.2' as const
+export const currentPatchLink = 'https://www.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-14-2-do-teamfight-tactics/' as const
