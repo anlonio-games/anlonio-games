a4ed49d6f45136d29367b1fb840db42f407d0d14
Bruno Donatelli <42386513+brunoredes@users.noreply.github.com>
2024-03-18 12:47:16 -0300

    chore: bump patch to 14.5

--- helper/currentPatch.ts
+++ helper/currentPatch.ts
@@ -1,2 +1,2 @@
-export const currentPatch = '14.4' as const
-export const currentPatchLink = 'https://www.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-14-4-do-teamfight-tactics/' as const
+export const currentPatch = '14.5' as const
+export const currentPatchLink = 'https://teamfighttactics.leagueoflegends.com/pt-br/news/game-updates/notas-da-atualizacao-14-5-do-teamfight-tactics/' as const
